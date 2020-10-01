d7f4bff88069357389428203ba4f42f14ad8c627
jashley-sg <65617568+jashley-sg@users.noreply.github.com>
2020-10-01 14:00:15 +0100

    Remove requests to api6.ipify.org (currently broken).

--- src/main.ts
+++ src/main.ts
@@ -13,13 +13,13 @@ export async function run(): Promise<void> {
 
   try {
     const ipv4 = await http.getJson<IPResponse>('https://api.ipify.org?format=json');
-    const ipv6 = await http.getJson<IPResponse>('https://api6.ipify.org?format=json');
+    //const ipv6 = await http.getJson<IPResponse>('https://api6.ipify.org?format=json');
 
     core.setOutput('ipv4', ipv4.result.ip);
-    core.setOutput('ipv6', ipv6.result.ip);
+    //core.setOutput('ipv6', ipv6.result.ip);
 
     core.info(`ipv4: ${ipv4.result.ip}`);
-    core.info(`ipv6: ${ipv6.result.ip}`);
+    //core.info(`ipv6: ${ipv6.result.ip}`);
   } catch (error) {
     core.setFailed(error.message);
   }
