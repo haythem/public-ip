8a56073b68a56affcef711f74222b7289dcddee6
Aubrey Stearn <aubrey@militantsouth.com>
2022-10-21 10:49:12 +0100

    bumped deps, set node version to 16.16.0 LTS, handled possible bug with error.message not existing. (#19)
    Co-authored-by: Aubrey Stearn <aubrey@woollen.io>

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,39 +1,40 @@
-import * as core from '@actions/core';
-import { HttpClient } from '@actions/http-client';
+import * as core from "@actions/core";
+import { HttpClient } from "@actions/http-client";
 
-import { run } from '../src/main';
+import { run } from "../src/main";
 
-describe('Public IP', () => {
-
+describe("Public IP", () => {
   beforeAll(() => {
-    jest.mock('@actions/http-client');
-    jest.spyOn(core, 'info');
-    jest.spyOn(core, 'getInput').mockReturnValue('6');
-    jest.spyOn(core, 'setFailed');
-    jest.spyOn(core, 'setOutput');
+    jest.mock("@actions/http-client");
+    jest.spyOn(core, "info");
+    jest.spyOn(core, "getInput").mockReturnValue("6");
+    jest.spyOn(core, "setFailed");
+    jest.spyOn(core, "setOutput");
   });
 
   afterAll(() => jest.resetAllMocks());
 
-  test('Return public ip address', async () => {
-    HttpClient.prototype.getJson = jest.fn()
-      .mockResolvedValue({ statusCode: 200, result: { ip: '1.2.3.4' } });
+  test("Return public ip address", async () => {
+    HttpClient.prototype.getJson = jest
+      .fn()
+      .mockResolvedValue({ statusCode: 200, result: { ip: "1.2.3.4" } });
 
-    await run();
+    await expect(run()).resolves.toBe(undefined);
 
     expect(HttpClient.prototype.getJson).toHaveBeenCalled();
-    expect(core.getInput).toHaveBeenCalledWith('maxRetries');
-    expect(core.getInput).toHaveReturnedWith('6');
+    expect(core.getInput).toHaveBeenCalledWith("maxRetries");
+    expect(core.getInput).toHaveReturnedWith("6");
     expect(core.setOutput).toHaveBeenCalledTimes(2);
-    expect(core.setOutput).toHaveBeenCalledWith('ipv4', '1.2.3.4');
-    expect(core.setOutput).toHaveBeenCalledWith('ipv6', '1.2.3.4');
+    expect(core.setOutput).toHaveBeenCalledWith("ipv4", "1.2.3.4");
+    expect(core.setOutput).toHaveBeenCalledWith("ipv6", "1.2.3.4");
   });
 
-  test('Fail when ipify does not respond', async () => {
-    HttpClient.prototype.getJson = jest.fn()
+  test("Fail when ipify does not respond", async () => {
+    HttpClient.prototype.getJson = jest
+      .fn()
       .mockRejectedValue({ statusCode: 500, result: null });
 
-    await run();
+    await expect(run()).resolves.toBe(undefined);
 
     expect(HttpClient.prototype.getJson).toHaveBeenCalled();
     expect(core.setFailed).toHaveBeenCalled();
