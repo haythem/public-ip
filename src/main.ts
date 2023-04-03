a4ede868d60397d6ff0f4b546f6487a3cc848feb
Rodrigo Gimenez <r.gimenez@insight-solutions.es>
2023-04-03 15:24:31 +0200

    feat: retry on error

--- src/main.ts
+++ src/main.ts
@@ -13,21 +13,31 @@ export async function run(): Promise<void> {
     maxRetries: maxRetries
   });
 
-  try {
-    const ipv4 = await http.getJson<IPResponse>(
-      "https://api.ipify.org?format=json"
-    );
-    const ipv6 = await http.getJson<IPResponse>(
-      "https://api64.ipify.org?format=json"
-    );
+  var numTries = 0;
+  var success = false;
+  while (!success && numTries < maxRetries) {
+    console.log(success);
+    try {
+      const ipv4 = await http.getJson<IPResponse>(
+        "https://api.ipify.org?format=json"
+      );
+      const ipv6 = await http.getJson<IPResponse>(
+        "https://api64.ipify.org?format=json"
+      );
 
-    core.setOutput("ipv4", ipv4.result.ip);
-    core.setOutput("ipv6", ipv6.result.ip);
+      core.setOutput("ipv4", ipv4.result.ip);
+      core.setOutput("ipv6", ipv6.result.ip);
 
-    core.info(`ipv4: ${ipv4.result.ip}`);
-    core.info(`ipv6: ${ipv6.result.ip}`);
-  } catch (error) {
-    core.setFailed(error?.message);
+      core.info(`ipv4: ${ipv4.result.ip}`);
+      core.info(`ipv6: ${ipv6.result.ip}`);
+      success = true;
+      console.log("success");
+    } catch (error) {
+      if (numTries == maxRetries - 1) {
+        core.setFailed(error?.message);
+      }
+    } 
+    numTries++;
   }
 }
 
